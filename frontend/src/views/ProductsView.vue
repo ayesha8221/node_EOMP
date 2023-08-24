254e9626bcd7781216e05711c0f94b0b67f869a3
Ayesha Galant <ayesha.glnt012@gmail.com>
2023-08-25 01:16:23 +0200

    search

--- frontend/src/views/ProductsView.vue
+++ frontend/src/views/ProductsView.vue
@@ -1,10 +1,47 @@
 <template>
-   <div>
+    <div>
+    <input type="text" v-model="searchQuery" @input="performSearch" placeholder="Search products🔎" />
+    <select v-model="selectedCategory" @change="filter">
+      <option value="">All</option>
+      <option v-for="category in categories" :key="category">{{ category }}</option>
+    </select>
+    <select v-model="sortOrder" @change="sortProducts">
+      <option value="asc">Low to High</option>
+      <option value="desc">High to Low</option>
+    </select>
+    <!-- Display filtered products -->
+    <div v-for="product in filteredProducts" :key="product.prodID">
+      <img :src="product.prodUrl" alt="Product Image" class="product-image" />
+        <h3>{{ product.prodName }}</h3>
+  <p>Price: R {{ product.amount }}</p>
+  <p>Quantity: {{ product.quantity }}</p>
+    </div>
+    
+
+    <div v-if="products" class="flex-container row row-cols-3">
+        <CardComp v-for="product of products" :key="product.prodID" :product="product" />
+    </div>
+    <div v-else class="loader"></div>
+  </div>
+
+   <!-- <div>
+    search
     <input type="text" v-model="searchQuery" @input="performSearch" placeholder="Search products🔎" />
     <div>
+
+      sort by price
+      <div>
+        
+      </div>
+
+<select v-model="sortOrder" @change="sortProducts">
+  <option value="asc">Low to High</option>
+  <option value="desc">High to Low</option>
+</select>
+      filter by category
 <select v-model="selectedCategory" @change="filter">
       <option value="">All</option>
-      <!-- <option value="Necklaces">Necklaces</option> -->
+      <option value="Necklaces">Necklaces</option>
       <option v-for="category in categories" :key="category">{{ category }}</option>
     </select>
 
@@ -21,7 +58,7 @@
     </div>
     
         <div v-else class="loader"></div>
-   </div>
+   </div> -->
 </template>
 <script>
 import CardComp from '@/components/CardComp.vue';
@@ -32,7 +69,8 @@ export default {
       selectedCategory: "", // Holds the selected category
       filteredProducts: [], // Holds the filtered products
       categories: ["Necklace", "Watch", "Earrings", "Ring"],
-      searchQuery: ""
+      searchQuery: "",
+      sortOrder: "asc"
     };
   },
 
@@ -49,34 +87,35 @@ export default {
       });
     },
 
-    sortProducts() {
-        this.filteredProducts.sort((a, b) => {
-            if (this.sortOrder === "asc") {
-                return a.amount - b.amount;
-            } else {
-                return b.amount - a.amount;
-            }
-        });
-    },
     performSearch() {
     this.filteredProducts = this.products.filter(product => {
       const productName = product.prodName.toLowerCase();
       return productName.includes(this.searchQuery.toLowerCase());
     });
+  },
+  sortProducts() {
+    this.products.sort((a, b) => {
+      if (this.sortOrder === "asc") {
+        return a.amount - b.amount;
+      } else {
+        return b.amount - a.amount;
+      }
+    });
+
+    // If products are currently being filtered, update the filteredProducts array
+    if (this.selectedCategory || this.searchQuery) {
+      this.filter();
+    }
   }
 
 
 
   },
-  mounted() {
-    this.$store.dispatch("getProducts").then(() => {
-      this.products = this.$store.state.products;
-    });
-  },
+
   
-    // mounted() {
-    //     this.$store.dispatch("getProducts")
-    // },
+    mounted() {
+        this.$store.dispatch("getProducts")
+    },
 
     components: {CardComp},
 }
