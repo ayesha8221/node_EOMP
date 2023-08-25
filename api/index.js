b18e18aab17e4b26e86bf626479eca0f4259e4b4
ayesha8221 <ayesha.glnt012@gmail.com>
2023-08-25 11:39:43 +0200

    link

--- api/index.js
+++ api/index.js
@@ -21,7 +21,7 @@ app.use(express.json(), cors(), cookieParser());
 
 // Allowing frontend access to backend
 app.use((req, res, next)=>{
-  res.header("Access-Control-Allow-Origin", "https://eomp-9b235.web.app/");
+  res.header("Access-Control-Allow-Origin", "");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Methods", "*");
   res.header("Access-Control-Request-Methods", "*");
