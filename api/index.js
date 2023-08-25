f98a04d58c8830122538860197a5fee00fa76535
ayesha8221 <ayesha.glnt012@gmail.com>
2023-08-25 11:50:14 +0200

    dff

--- api/index.js
+++ api/index.js
@@ -21,7 +21,7 @@ app.use(express.json(), cors(), cookieParser());
 
 // Allowing frontend access to backend
 app.use((req, res, next)=>{
-  res.header("Access-Control-Allow-Origin", "");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Methods", "*");
   res.header("Access-Control-Request-Methods", "*");
